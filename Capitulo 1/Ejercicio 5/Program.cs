f16e9b42128cd0160be13e13db34e9a52139bb68
JhordyGuzman <70299011+JhordyGuzman@users.noreply.github.com>
2020-09-11 19:56:33 -0400

    Add files via upload

---
+++ Capitulo 1/Ejercicio 5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ejercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Buenas Tardes");
+            Console.WriteLine("Bienvenido a el curso de C#");
+            Console.WriteLine("Espero que les sirva de mucho");
+            Console.WriteLine("Nos Vemos");
+        }
+    }
+}
