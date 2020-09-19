d9d30eee91afacee349cb5b4f2a9d906907b5b13
JhordyGuzman <70299011+JhordyGuzman@users.noreply.github.com>
2020-09-19 18:03:00 -0400

    Add files via upload

---
+++ Tarea 2/Ejercicios/Ejercicio4_1.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Tarea_2.Ejercicios
+{
+    public class Ejercicio4_1
+    {
+        public static void Ejercicio()
+        {
+            int number = 0;
+            Console.WriteLine("Ingrese el numero del cual quiere ver su tabla: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+                Console.WriteLine($"{i} X {number} = {number * i}");
+        }
+    }
+}
---
+++ Tarea 2/Ejercicios/Ejercicio4_2.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Tarea_2.Ejercicios
+{
+    public class Ejercicio4_2
+    {
+        public static void Ejercicio()
+        {
+            int number, power;
+            Console.WriteLine("Ingrese el numero el cual desea potenciar: ");
+
+            number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la potencia: ");
+
+            power = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{number}^{power} = {Math.Pow(number, power)}");
+
+        }
+    }
+}
